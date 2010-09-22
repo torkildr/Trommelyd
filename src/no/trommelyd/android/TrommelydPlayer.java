99cc5419f5cf1eb84d7c3786ddaf86c5ce6dba0d
Torkild Retvedt <torkild@retvedt.no>
2010-09-22 09:06:38 +0200

    Fixed tabs, should make Eclipse behave later..

--- src/no/trommelyd/android/TrommelydPlayer.java
+++ src/no/trommelyd/android/TrommelydPlayer.java
@@ -17,65 +17,65 @@ import android.view.View.OnClickListener;
  */
 class TrommelydPlayer implements OnClickListener, OnCompletionListener, OnPreparedListener {
 
-	private MediaPlayer player;
-	
-	private Context context;
+    private MediaPlayer player;
+    
+    private Context context;
 
-	// For some reason, we want to create the media player
-	private synchronized void createMediaPlayer() {
+    // For some reason, we want to create the media player
+    private synchronized void createMediaPlayer() {
         player = MediaPlayer.create(context, R.raw.trommelyd);
-	}
+    }
 
-	// Start playing sound
-	@Override
-	public synchronized void onClick(View v) {
-		// If we don't know what sound to play, simply don't play it...
-		if (player == null) {
-			return;
-		}
-		
-		// Either restart to start playing media (we restart so that we don't
-		// get a bunch of queued plays that will go on forever)
-    	if (player.isPlaying()) {
-    		player.seekTo(0);
-    	} else {
-	    	player.start();
-    	}
-	}
+    // Start playing sound
+    @Override
+    public synchronized void onClick(View v) {
+        // If we don't know what sound to play, simply don't play it...
+        if (player == null) {
+            return;
+        }
+        
+        // Either restart to start playing media (we restart so that we don't
+        // get a bunch of queued plays that will go on forever)
+        if (player.isPlaying()) {
+            player.seekTo(0);
+        } else {
+            player.start();
+        }
+    }
 
-	// Prepare next play when completed
-	@Override
-	public synchronized void onCompletion(MediaPlayer player) {
-    	player.stop();
-    	
-    	// This will block (and since we're in a synch'ed method, this is 'good')
-    	try {
-    		player.prepare();
-    	} catch (IOException e) {
-    		// meh
-    		return;
-    	}
-	}
+    // Prepare next play when completed
+    @Override
+    public synchronized void onCompletion(MediaPlayer player) {
+        player.stop();
+        
+        // This will block (and since we're in a synch'ed method, this is 'good')
+        try {
+            player.prepare();
+        } catch (IOException e) {
+            // meh
+            return;
+        }
+    }
 
-	// Finished preparing, seek to start of file
-	@Override
-	public void onPrepared(MediaPlayer player) {
-		player.seekTo(0);
-	}
-	
-	// Load media for first play
-	public TrommelydPlayer(Context context) {
-		// For safe-keeping
-		this.context = context;
-		
-		// Load media file
-		createMediaPlayer();
+    // Finished preparing, seek to start of file
+    @Override
+    public void onPrepared(MediaPlayer player) {
+        player.seekTo(0);
+    }
+    
+    // Load media for first play
+    public TrommelydPlayer(Context context) {
+        // For safe-keeping
+        this.context = context;
+        
+        // Load media file
+        createMediaPlayer();
 
         // When playback is complete, prepare for next play
-    	player.setOnCompletionListener(this);
-    	
-    	// When media is prepared, make sure position is correct
-    	player.setOnPreparedListener(this);
-	}
-	
+        player.setOnCompletionListener(this);
+        
+        // When media is prepared, make sure position is correct
+        player.setOnPreparedListener(this);
+    }
+    
 }
